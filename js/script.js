9234d514c8453349042380f9f6780f7ffefcba8d
Guilherme Alves Lumertz <glumertz@plataformaa.com.br>
2024-09-05 19:44:12 -0300

    accordion and slider section - ok

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// HEADER SCROLL
+
 var className = "scroll-header";
 var scrollTrigger = 60;
 
@@ -9,10 +11,81 @@ window.onscroll = function () {
   }
 };
 
+// MENU ANIMATION
+
 let burger = document.getElementById("navTrigger"),
   nav = document.getElementById("navMenu");
 
 burger.addEventListener("click", function (e) {
   this.classList.toggle("active");
   nav.classList.toggle("active");
+});
+
+// ACCORDION
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item");
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+
+    const currentlyActiveAccordionItemHeader = document.querySelector(
+      ".accordion-item.active"
+    );
+    if (
+      currentlyActiveAccordionItemHeader &&
+      currentlyActiveAccordionItemHeader !== accordionItemHeader
+    ) {
+      currentlyActiveAccordionItemHeader.classList.toggle("active");
+      currentlyActiveAccordionItemHeader.querySelector(
+        ".accordion-item-body"
+      ).style.maxHeight = 0;
+    }
+
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.querySelector(
+      ".accordion-item-body"
+    );
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
+});
+
+// SLIDER
+
+var swiper = new Swiper(".slide-cards", {
+  slidesPerView: 3,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".s-cards .swiper-button-next",
+    prevEl: ".s-cards .swiper-button-prev",
+  },
+  pagination: {
+    el: ".s-cards .swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.3,
+      spaceBetween: 16,
+    },
+    600: {
+      slidesPerView: 2.3,
+      spaceBetween: 15,
+    },
+    1050: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
+});
+
+// SCROLL ANIMATION
+
+AOS.init({
+  duration: 1000,
+  once: true,
 });
